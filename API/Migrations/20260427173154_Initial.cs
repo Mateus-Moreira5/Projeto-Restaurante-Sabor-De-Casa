501dc8cd7e5e85d4a1108b3e04365c3b92cf1622
Mateus Moreira Fernandes <mateusmoreirafernandes75@gmail.com>
2026-04-27 14:37:36 -0300

    Migrations criadas e bando de dados rodando todo bunitin com as relações corretas

---
+++ API/Migrations/20260427173154_Initial.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FormulariosPedido",
+                columns: table => new
+                {
+                    CodigoPedido = table.Column<Guid>(type: "uuid", nullable: false),
+                    Nome = table.Column<string>(type: "character varying(35)", maxLength: 35, nullable: false),
+                    Telefone = table.Column<string>(type: "character varying(12)", maxLength: 12, nullable: false),
+                    TipoPagamento = table.Column<string>(type: "text", nullable: false),
+                    TipoEntrega = table.Column<string>(type: "text", nullable: false),
+                    Cep = table.Column<string>(type: "character varying(8)", maxLength: 8, nullable: true),
+                    Logradouro = table.Column<string>(type: "text", nullable: true),
+                    Numero = table.Column<string>(type: "text", nullable: true),
+                    Bairro = table.Column<string>(type: "text", nullable: true),
+                    Complemento = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormulariosPedido", x => x.CodigoPedido);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pratos",
+                columns: table => new
+                {
+                    IdPrato = table.Column<Guid>(type: "uuid", nullable: false),
+                    NomePrato = table.Column<string>(type: "character varying(35)", maxLength: 35, nullable: false),
+                    Descricao = table.Column<string>(type: "text", nullable: true),
+                    InfoIngredientes = table.Column<string>(type: "text", nullable: false),
+                    Acompanhamentos = table.Column<string>(type: "text", nullable: false),
+                    Preco = table.Column<decimal>(type: "numeric(8,2)", nullable: false),
+                    Disponivel = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pratos", x => x.IdPrato);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItensPedido",
+                columns: table => new
+                {
+                    IdPedido = table.Column<Guid>(type: "uuid", nullable: false),
+                    FkCodigoPedido = table.Column<Guid>(type: "uuid", nullable: false),
+                    FkIdPrato = table.Column<Guid>(type: "uuid", nullable: false),
+                    Quantidade = table.Column<int>(type: "integer", nullable: false),
+                    Personalizacao = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItensPedido", x => x.IdPedido);
+                    table.ForeignKey(
+                        name: "FK_ItensPedido_FormulariosPedido_FkCodigoPedido",
+                        column: x => x.FkCodigoPedido,
+                        principalTable: "FormulariosPedido",
+                        principalColumn: "CodigoPedido",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ItensPedido_Pratos_FkIdPrato",
+                        column: x => x.FkIdPrato,
+                        principalTable: "Pratos",
+                        principalColumn: "IdPrato",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItensPedido_FkCodigoPedido",
+                table: "ItensPedido",
+                column: "FkCodigoPedido");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItensPedido_FkIdPrato",
+                table: "ItensPedido",
+                column: "FkIdPrato");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItensPedido");
+
+            migrationBuilder.DropTable(
+                name: "FormulariosPedido");
+
+            migrationBuilder.DropTable(
+                name: "Pratos");
+        }
+    }
+}
--- API/data/AppDbContext.cs
+++ API/data/AppDbContext.cs
@@ -10,4 +10,9 @@ public class AppDbContext : DbContext
     public DbSet<Prato> Pratos { get; set; }
     public DbSet<FormularioPedido> FormulariosPedido { get; set; }
     public DbSet<Pedido> ItensPedido { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
 }
