0dde9b650f99751b07abd835b878dbc4f6af1092
Mateus Moreira Fernandes <mateusmoreirafernandes75@gmail.com>
2026-05-14 15:32:43 -0300

    testando

--- API/Program.cs
+++ API/Program.cs
@@ -13,11 +13,18 @@ using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Configuration.AddEnvironmentVariables();
+
 builder.Services.AddOpenApi();
+
 builder.Services.AddControllers();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
 
 builder.Services.AddCors(options =>
 {
@@ -31,15 +38,20 @@ builder.Services.AddCors(options =>
 
 builder.Services.AddScoped<IPratoRepository, PratoRepository>();
 builder.Services.AddScoped<IPratoService, PratoService>();
+
 builder.Services.AddScoped<IFormularioPedidoRepository, FormularioPedidoRepository>();
 builder.Services.AddScoped<IFormularioPedidoService, FormularioPedidoService>();
+
 builder.Services.AddScoped<IPedidoRepository, PedidoRepository>();
 builder.Services.AddScoped<IPedidoService, PedidoService>();
-builder.Services.AddValidatorsFromAssemblyContaining<Program>();
-builder.Services.AddFluentValidationAutoValidation();
-builder.Services.AddHttpClient<ViaCepService>();
+
 builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+builder.Services.AddFluentValidationAutoValidation();
+
+builder.Services.AddHttpClient<ViaCepService>();
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
@@ -50,24 +62,40 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidateAudience = true,
             ValidateLifetime = true,
             ValidateIssuerSigningKey = true,
+
             ValidIssuer = builder.Configuration["Jwt:Issuer"],
             ValidAudience = builder.Configuration["Jwt:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(
+                    builder.Configuration["Jwt:Key"]!
+                )
+            )
         };
     });
+
 builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 app.MapOpenApi();
+
 app.MapScalarApiReference();
 
 app.Map("/", () => Results.Redirect("/scalar"));
+
 app.UseCors("AllowAll");
+
 app.UseAuthentication();
+
 app.UseAuthorization();
-app.UseMiddleware<ExceptionMiddleware>();   
+
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.MapControllers();
+
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 
 app.Urls.Add($"http://0.0.0.0:{port}");
+
 app.Run();
