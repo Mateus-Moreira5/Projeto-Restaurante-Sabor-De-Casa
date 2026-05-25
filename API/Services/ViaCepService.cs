4e020a2d7123e713150d96aa4bc3d83ea47dde94
Mateus <76483627+Mateus-Moreira5@users.noreply.github.com>
2026-05-25 19:00:35 -0300

    Change API URL from HTTPS to HTTP for ViaCepService

--- API/Services/ViaCepService.cs
+++ API/Services/ViaCepService.cs
@@ -7,7 +7,7 @@ public class ViaCepService (HttpClient httpClient)
     public async Task<ViaCepResponseDto?> BuscarEnderecoAsync(string cep)
     {
         var cepLimpo = cep.Replace("-", "").Trim();
-        var response = await httpClient.GetAsync($"https://viacep.com.br/ws/{cepLimpo}/json/");
+        var response = await httpClient.GetAsync($"http://viacep.com.br/ws/{cepLimpo}/json/");
         if (!response.IsSuccessStatusCode) return null;
         var content = await response.Content.ReadFromJsonAsync<ViaCepResponseDto>();
         if (content is null || content.Erro == true) return null;
